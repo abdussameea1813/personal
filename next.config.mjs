62eeeb64a65a152ba0e05317a7645b0bba99bcaf
abdussameea1813 <abdussameeapatel18@gmail.com>
2025-10-26 12:18:39 -0400

    Prepare for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
   experimental: {
     optimizeCss: true,
   },
-}
+  assetPrefix: isProd ? '/personal/' : '',
+  basePath: isProd ? '/personal' : '',
+};
 
-export default nextConfig
+export default nextConfig;
